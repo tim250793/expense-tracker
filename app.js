d8ddfbf63abd8ee39c6f5bcd6b043e858788b33b
tim <timthegoodness@gmail.com>
2022-10-03 16:24:05 +0800

    creat views register

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+
 // handlebars 樣板引擎
 const exphbs = require('express-handlebars')
 // mongoose 連線狀態
